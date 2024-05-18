d006c9df6bb2515580b8ad369302d1e6270fd1ba
syagawa <syagawapp@gmail.com>
2024-05-18 23:07:59 +0900

    Clean code

--- usb_key_sender/src/main.cpp
+++ usb_key_sender/src/main.cpp
@@ -42,63 +42,17 @@ static bool onStartStop(uint8_t power_condition, bool start, bool load_eject){
   return true;
 }
 
-int32_t mscWriteCallback (uint32_t lba, uint8_t* buffer, uint32_t bufsize)
-{
-  uint8_t* addr = msc_disk[lba];
-  memcpy(addr, buffer, bufsize);
-
-  flickLed(2, "red");
-
-  delay(500);
-  
-  writeFlg = 1;
-  return bufsize;
-}
-
-int32_t mscReadCallback (uint32_t lba, void* buffer, uint32_t bufsize)
-{
-  uint8_t const* addr = msc_disk[lba];
-  memcpy(buffer, addr, bufsize);
-
-  flickLed(2, "blue");
-
-  readFlg = 1;
-  return bufsize;
-}
-
 static int32_t onWrite(uint32_t lba, uint32_t offset, uint8_t* buffer, uint32_t bufsize){
-  // USBSerial.printf("1MSC WRITE: lba: %u, offset: %u, bufsize: %u\n", lba, offset, bufsize);
-  // USBSerial.printf("2MSC WRITE buffer: %u\n", buffer);
-
-  // memcpy(buf1, buf2, n)
-  // void *buf1　：　コピー先のメモリブロック
-  // const void *buf2　：　コピー元のメモリブロック
-  // size_t n　：　コピーバイト数
-  // このmemcpyは動いていない？ この行が無くてもREADME.txtは表示されるし、データの更新もできる(電源が入ってる限り)
   memcpy(msc_disk[lba] + offset, buffer, bufsize);
-
   flickLed(2, "red");
-
-
   writeFlg = 1;
   return bufsize;
 }
 
 static int32_t onRead(uint32_t lba, uint32_t offset, void* buffer, uint32_t bufsize){
-  // USBSerial.printf("1MSC READ: lba: %u, offset: %u, bufsize: %u\n", lba, offset, bufsize);
-  // USBSerial.printf("2MSC READ buffer: %u\n", buffer);
-
-  // memcpy(buf1, buf2, n)
-  // void *buf1　：　コピー先のメモリブロック
-  // const void *buf2　：　コピー元のメモリブロック
-  // size_t n　：　コピーバイト数
-  // このmemcpyは動いている この行が無いとREADME.txtファイルもストレージも表示されない
   memcpy(buffer, msc_disk[lba] + offset, bufsize);
-
   flickLed(2, "yellow");
-
   readFlg = 1;
-
   return bufsize;
 }
 
@@ -151,8 +105,7 @@ int bootmode = 0;
 // regular mode settings >>
 
 //// initial settings
-char * initialContents = R"({"settings_mode": "storage", "color": "red", "key": "abc"})";
-String settings_mode = "storage";
+char * initialContents = R"({"color": "red", "keys": ["abc"]})";
 int requiresResetInSettingsMode = 0;
 
 //// regular code in setup
@@ -251,67 +204,54 @@ void loopInRegularMode(){
 // regular mode settings <<
 
 void setupInSettingsMode(){
-    if(settingsDoc.containsKey("settings_mode")){
-      settings_mode = settingsDoc["settings_mode"].as<String>();
-    }
+    // storage
+    USB.onEvent(usbEventCallback);
+    MSC.vendorID("M5AtomS3U");//max 8 chars
+    MSC.productID("USB_MSC");//max 16 chars
+    MSC.productRevision("1.0");//max 4 chars
+    MSC.onStartStop(onStartStop);
+    MSC.onRead(onRead);
+    MSC.onWrite(onWrite);
+    MSC.mediaPresent(true);
+    MSC.begin(DISK_SECTOR_COUNT, DISK_SECTOR_SIZE);
 
-    if(settings_mode == "storage"){
-      // storage
-      USB.onEvent(usbEventCallback);
-
-      // MSC.setID("M5AtomS3U", "USBD_MSC", "1.0");
-      // MSC.setCapacity(DISK_SECTOR_COUNT, DISK_SECTOR_SIZE);
-      // // MSC.setReadyCallback(msc_ready_cb);
-      // MSC.setStartStopCallback(onStartStop);
-      // MSC.setReadWriteCallback(mscReadCallback, mscWriteCallback, flush_cb);
-      // MSC.setUnitReady(true);
-      // MSC.begin();
-      MSC.vendorID("M5AtomS3U");//max 8 chars
-      MSC.productID("USB_MSC");//max 16 chars
-      MSC.productRevision("1.0");//max 4 chars
-      MSC.onStartStop(onStartStop);
-      MSC.onRead(onRead);
-      MSC.onWrite(onWrite);
-      MSC.mediaPresent(true);
-      MSC.begin(DISK_SECTOR_COUNT, DISK_SECTOR_SIZE);
-
-      USB.begin();
-    }
+    USB.begin();
+    
 }
 
 void loopInSettingsMode(){
-  if(settings_mode == "storage"){
-    if(writeFlg == 1){
-      int targetSize = sizeof(msc_disk[3]);
-      String str = (char *) msc_disk[3];
-      int strsize = str.length();
-      delay(100);
+  
+  if(writeFlg == 1){
+    int targetSize = sizeof(msc_disk[3]);
+    String str = (char *) msc_disk[3];
+    int strsize = str.length();
+    delay(100);
 
-      writeToFile(str);
-      delay(100);
+    writeToFile(str);
+    delay(100);
 
-      writeFlg = 0;
-    }
-    if(readFlg == 1){
-      readFlg = 0;
-    }
+    writeFlg = 0;
+  }
+  if(readFlg == 1){
+    readFlg = 0;
+  }
 
-    if(setLedStr != ""){
-      char result[10];
-      setLedStr.toCharArray(result, 10);
+  if(setLedStr != ""){
+    char result[10];
+    setLedStr.toCharArray(result, 10);
 
-      liteLed(setLedStr);
-      setLedStr = "";
-    }
+    liteLed(setLedStr);
+    setLedStr = "";
+  }
 
 
-    if(readFlg != 1 && writeFlg != 1 && M5.BtnA.wasPressed()){
-      // If you press the button five times within 5 seconds, it will delete the files in the ROM area and restart. The configuration JSON will be reset to its initial state.
-      if(pressAndCheckBtnPressedXTimesWithinYSedonds(5, 5)){
-        requiresResetInSettingsMode = 1;
-      }
+  if(readFlg != 1 && writeFlg != 1 && M5.BtnA.wasPressed()){
+    // If you press the button five times within 5 seconds, it will delete the files in the ROM area and restart. The configuration JSON will be reset to its initial state.
+    if(pressAndCheckBtnPressedXTimesWithinYSedonds(5, 5)){
+      requiresResetInSettingsMode = 1;
     }
   }
+  
 
   if(requiresResetInSettingsMode == 1){
     resetAndRestart();
